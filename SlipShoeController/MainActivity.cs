d471f30e41895a9ed585dabd64d824fcd2cb9aa5
Andrew Butler <andrewbutler@unomaha.edu>
2020-01-23 21:47:13 -0600

    Add project files.

---
+++ SlipShoeController/MainActivity.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Android.Bluetooth;
+using Java.Util;
+using System.Text;
+using System.IO;
+
+namespace SlipShoeController
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        TextView Data;
+        Button Send, Connect;
+        TextInputEditText MessageToSend;
+        BluetoothAdapter BTAdapter = BluetoothAdapter.DefaultAdapter;
+        BluetoothDevice BTDevice;
+        BluetoothSocket BTSocket;
+        Thread BTThread;
+
+        bool Connected = false;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            //Get the UI elements as objects
+            MessageToSend = FindViewById<TextInputEditText>(Resource.Id.MessageToSend);
+            Send = FindViewById<Button>(Resource.Id.Send);
+            Connect = FindViewById<Button>(Resource.Id.Connect);
+            Data = FindViewById<TextView>(Resource.Id.data);
+
+            //Set event methods for button clicks
+            Send.Click += OnSendClick;
+            Connect.Click += OnConnectClick;
+        }
+
+        //Open the connection to the bluetooth device
+        private void OnConnectClick(object sender, EventArgs eventArgs)
+        {
+            Data.Append("\nConnecting\n");
+
+            //Find the desired module from the list
+            BTDevice = (from bd in BTAdapter.BondedDevices where bd.Name == "DSD TECH HC-05" select bd).FirstOrDefault();
+
+            if(!Connected)
+            {
+                //Create a socket to communicate through
+                BTSocket = BTDevice.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+                BTSocket.Connect();
+
+                //Start the background thread listening for data incoming from bluetooth
+                BTThread = new Thread(() => { BluetoothListener(); });
+                BTThread.Start();
+                Connected = true;
+            }
+            
+        }
+
+        private void BluetoothListener()
+        {
+            try
+            {
+                var InputStream = (BTSocket.InputStream as InputStreamInvoker).BaseInputStream;
+
+                while (true)
+                {                
+
+                    if(InputStream.Available() > 0)
+                    {
+                        byte[] buf = new byte[InputStream.Available()];
+
+                        InputStream.Read(buf);
+
+                        string data = Encoding.UTF8.GetString(buf);
+
+                        if (data.Length > 0)
+                        {
+                            this.RunOnUiThread(() =>
+                            {
+                                Data.Text += data;
+                            });
+                        }
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (ThreadAbortException) { }
+        }
+
+        private void OnSendClick(object sender, EventArgs eventArgs)
+        {
+            Data.Append("\nSending: " + MessageToSend.Text + "\n");
+
+            byte[] buffer = Encoding.UTF8.GetBytes(MessageToSend.Text);
+
+            BTSocket.OutputStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        //private void FabOnClick(object sender, EventArgs eventArgs)
+        //{
+        //    View view = (View) sender;
+        //    Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+        //        .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        //}
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
+
